b41e6a294c331ab2ccbfc0cf95340915bec00d0c
pragnakar <pragnakar@gmail.com>
2016-11-29 17:56:58 -0500

    comit

--- v1.R
+++ v1.R
@@ -17,12 +17,14 @@ library(plyr)
 
 library(quantmod)
 
+#get the company name from user 
 company_name = readline(" enter company name :\n")
 
 stock_info = getOptionChain(company_name, Exp = NULL, src = "yahoo", "2016/2017")
-
+######################################################
 
 month_wise_data = stock_info$Apr.21.2017
+#picking which month data one wants to work on 
 list_of_contents = names(stock_info)
 for (i in 1:length(list_of_contents)) {
     cat(i, ") ", list_of_contents[i], " \n")
@@ -38,11 +40,25 @@ option_data = stock_info[[input]]
 View(option_data)
 names(option_data)
 
-
+################################################ company data 
 company_details = getQuote(company_name)
 spot_price = company_details$Last # spot price 
-company_details$Change
+#################################################### volatility 
+
+#####################################################
+
+# volatility of asset - sigma 
+
+library(tseries)
+data <- get.hist.quote(company_name, origin = )
+price <- data$Close
+ret <- log(lag(price)) - log(price)
+sigma <- sd(ret) * sqrt(252) * 100
+sigma <- sigma / 100
+##########################################################################
+# pick whether to work on call or put options 
 choice = readline(" enter whether you want to work on \ncalls or options for call press 1 for put press 2:\n ")
+
 if (choice == 1) {
     # call options 
 call_option_list = option_data$calls
