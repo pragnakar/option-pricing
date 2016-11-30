05f691e836f8c19ae1baaad8e39a0a142bc2a212
pragnakar <pragnakar@gmail.com>
2016-11-30 10:50:32 -0500

    comit

--- v1.R
+++ v1.R
@@ -10,20 +10,11 @@ clear_screen <- function() {
 }
 
 ################### get data 
-
-library(RCurl)
- library(jsonlite)
-library(plyr)
-
 library(quantmod)
-
 #get the company name from user 
-company_name = readline(" enter company name :\n")
-
+company_name = readline( prompt =" enter company name :")
 stock_info = getOptionChain(company_name, Exp = NULL, src = "yahoo", "2016/2017")
 ######################################################
-
-month_wise_data = stock_info$Apr.21.2017
 #picking which month data one wants to work on 
 list_of_contents = names(stock_info)
 for (i in 1:length(list_of_contents)) {
@@ -31,18 +22,14 @@ for (i in 1:length(list_of_contents)) {
 
 
 }
-
 input = as.integer(readline("enter on which month data you want to work on.\n type the row number : \n"))
-
-a = stock_info$Dec.02.2016
-View(a)
 option_data = stock_info[[input]]
-View(option_data)
 names(option_data)
 
 ################################################ company data 
 company_details = getQuote(company_name)
-spot_price = company_details$Last # !!!! spot price 
+spot_price = company_details$Last # !!!! spot price
+cat("spot price: ", spot_price)
 #################################################### volatility 
 
 #####################################################
@@ -57,11 +44,13 @@ price <- data$Close
 ret <- log(lag(price)) - log(price)
 sigma <- sd(ret) * sqrt(252) * 100
 sigma <- sigma / 100 #!!!! sigma
+cat(" sigma :", sigma)
 ################################################
 
 # risk free rate 
 
 risk_free_rate = 0.03
+cat("risk free rate set as :", risk_free_rate)
 ######################################################
 # date 
 expiry_date = as.Date(list_of_contents[input], format = "%b.%d.%Y")
@@ -69,35 +58,36 @@ expiry_date = as.Date(list_of_contents[input], format = "%b.%d.%Y")
 year_to_expire = as.numeric(expiry_date - today) / 365 # time 
 days_to_expire = as.numeric(expiry_date - today)
 
+cat(" time of option to mature : ", year_to_expire)
 ##########################################################################
 # pick whether to work on call or put options 
-choice = readline(" enter whether you want to work on \ncalls or options for call press 1 for put press 2:\n ")
+choice = readline(" enter whether you want to work on \ncalls or options for call press 1 and for put press 2:\n ")
 
 if (choice == 1) {
     # call options 
-call_option_list = option_data$calls
-View(call_option_list)
-input  = as.numeric(readline("enter which option you want to work on : \n"))
-data_option = call_option_list[input,]
+    call_option_list = option_data$calls
+    View(call_option_list)
+    input = as.numeric(readline("enter which option you want to work on : \n"))
+    data_option = call_option_list[input,]
 
-strike = data_option$Strike # !!!! strike 
-price_option = data_option$Last # !!!!! option price 
+    strike = data_option$Strike # !!!! strike 
+    price_option = data_option$Last # !!!!! option price 
 
 
 
 
     ############## price calculation 
-    T = year_to_expire 
+    T = year_to_expire
     K = strike
     r = risk_free_rate
     S0 = spot_price
 
-d1 <- (log(S0 / K) + (r + sigma ^ 2 / 2) * T) / (sigma * sqrt(T))
-d2 <- d1 - sigma * sqrt(T)
-phid1 <- pnorm(d1)
-price_call <- S0 * phid1 - K * exp( - r * T) * pnorm(d2)
-price_call
-price_option
+    d1 <- (log(S0 / K) + (r + sigma ^ 2 / 2) * T) / (sigma * sqrt(T))
+    d2 <- d1 - sigma * sqrt(T)
+    phid1 <- pnorm(d1)
+    estimated_price <- S0 * phid1 - K * exp( - r * T) * pnorm(d2)
+    cat(" estimated price of call option : ", estimated_price)
+    cat(" price of call option on market ", price_option)
 
 
 
@@ -105,17 +95,17 @@ price_option
 
 
 } else {
-# put options 
+    # put options 
 
-put_option_list = option_data$puts
-View(put_option_list)
+    put_option_list = option_data$puts
+    View(put_option_list)
     input = as.numeric(readline("enter which option you want to work on : \n"))
     data_option = put_option_list[input,]
 
     strike = data_option$Strike # !!!! strike 
     price_option = data_option$Last # !!!!! option price 
 
-    
+
 
 
     ############## price calculation 
@@ -127,14 +117,45 @@ View(put_option_list)
     d1 <- (log(S0 / K) + (r + sigma ^ 2 / 2) * T) / (sigma * sqrt(T));
     d2 <- d1 - sigma * sqrt(T);
     phimd1 <- pnorm( - d1)
-    price_put <- -S0 * phimd1 + K * exp( - r * T) * pnorm( - d2);
-    price_put
-    price_option
+    estimated_price <- -S0 * phimd1 + K * exp( - r * T) * pnorm( - d2);
+    cat("estimated price of put option ", estimated_price)
+    cat("price of put option on market ", price_option)
 
 
 
 
 }
+
+
+##################################################################################
+# monte carlo simulation 
+stock = S0
+strike = K
+TTM = T
+rf = r
+num.sim <- 100000R <- (rf - 0.5 * sigma ^ 2) * TTM
+SD <- sigma * sqrt(TTM)TTM.price <- stock * exp(R + SD * rnorm(num.sim, 0, 1))
+
+# call option 
+TTM.call <- pmax(0, TTM.price - strike)
+PV.call <- TTM.call * (exp( - rf * TTM))
+mean(PV.call)
+
+# put  option 
+
+TTM.put <- pmax(0, strike - TTM.price)PV.put <- TTM.put * (exp( - rf * TTM))mean(PV.put)
+
+
+
+
+
+
+
+
+
+
+
+##################################################################################
 
 stock_info[[1]]
 names(stock_info)
