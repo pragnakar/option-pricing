a03ad52b5dfb0cd75eaa477ea2c21623718f0df3
pragnakar <pragnakar@gmail.com>
2016-11-29 19:09:05 -0500

    comit

--- v1.R
+++ v1.R
@@ -42,19 +42,33 @@ names(option_data)
 
 ################################################ company data 
 company_details = getQuote(company_name)
-spot_price = company_details$Last # spot price 
+spot_price = company_details$Last # !!!! spot price 
 #################################################### volatility 
 
 #####################################################
 
 # volatility of asset - sigma 
+today = Sys.Date()
 
+previous_year = as.Date(seq(today, length = 2, by = "-1 years")[2])
 library(tseries)
-data <- get.hist.quote(company_name, origin = )
+data <- get.hist.quote(company_name, start = previous_year)
 price <- data$Close
 ret <- log(lag(price)) - log(price)
 sigma <- sd(ret) * sqrt(252) * 100
-sigma <- sigma / 100
+sigma <- sigma / 100 #!!!! sigma
+################################################
+
+# risk free rate 
+
+risk_free_rate = 0.03
+######################################################
+# date 
+expiry_date = as.Date(list_of_contents[input], format = "%b.%d.%Y")
+
+year_to_expire = as.numeric(expiry_date - today) / 365 # time 
+days_to_expire = as.numeric(expiry_date - today)
+
 ##########################################################################
 # pick whether to work on call or put options 
 choice = readline(" enter whether you want to work on \ncalls or options for call press 1 for put press 2:\n ")
@@ -63,15 +77,31 @@ if (choice == 1) {
     # call options 
 call_option_list = option_data$calls
 View(call_option_list)
-data_option = call_option_list
+input  = as.numeric(readline("enter which option you want to work on : \n"))
+data_option = call_option_list[input,]
 
-strike = data_option$Strike # strike 
-price_option = data_option$Last # option price 
+strike = data_option$Strike # !!!! strike 
+price_option = data_option$Last # !!!!! option price 
 
-expiry_date = as.Date( list_of_contents[input], format = "%b.%d.%Y")
 
-year_to_expire = as.numeric(expiry_date - today) / 365 # time 
-days_to_expire = as.numeric(expiry_date - today)
+
+
+    ############## price calculation 
+    T = year_to_expire 
+    K = strike
+    r = risk_free_rate
+    S0 = spot_price
+
+d1 <- (log(S0 / K) + (r + sigma ^ 2 / 2) * T) / (sigma * sqrt(T))
+d2 <- d1 - sigma * sqrt(T)
+phid1 <- pnorm(d1)
+price_call <- S0 * phid1 - K * exp( - r * T) * pnorm(d2)
+price_call
+price_option
+
+
+
+
 
 
 } else {
@@ -79,6 +109,29 @@ days_to_expire = as.numeric(expiry_date - today)
 
 put_option_list = option_data$puts
 View(put_option_list)
+    input = as.numeric(readline("enter which option you want to work on : \n"))
+    data_option = put_option_list[input,]
+
+    strike = data_option$Strike # !!!! strike 
+    price_option = data_option$Last # !!!!! option price 
+
+    
+
+
+    ############## price calculation 
+    T = year_to_expire
+    K = strike
+    r = risk_free_rate
+    S0 = spot_price
+
+    d1 <- (log(S0 / K) + (r + sigma ^ 2 / 2) * T) / (sigma * sqrt(T));
+    d2 <- d1 - sigma * sqrt(T);
+    phimd1 <- pnorm( - d1)
+    price_put <- -S0 * phimd1 + K * exp( - r * T) * pnorm( - d2);
+    price_put
+    price_option
+
+
 
 
 }
